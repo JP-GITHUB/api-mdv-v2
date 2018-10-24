e179669d325f7f41cf55f190caae80f4e2cb3c5c
EvelynMC <ecmcaceres@gmail.com>
2018-10-24 20:35:59 -0300

    Update routers/users
    Se agregan validaciones

--- routes/users.js
+++ routes/users.js
@@ -32,16 +32,44 @@ router.delete('/', middle_auth.validate, async function (req, res, next) {
     res.json(await user_ctr.delete(req.body.id));
 });
 
-router.post('/register',  async function (req, res, next) {
+//Registro
+router.post('/register', [
+    check('name').isEmpty().isLength({ min: 3 }),
+    check('mail').isEmail().isEmpty(),
+    check('lastname').isEmpty().isLength({ min: 3}),
+    check('rut').isEmpty(),
+    check('telephone').isEmpty().isLength({ max: 12}),
+    check('password').isEmpty().isLength({ min: 4})
+], async function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
     let data = req.body;
     res.json(await user_ctr.register(data));
 });
 
-router.post('/forgot_password', async function (req, res) {
+//Recuperar contraseña
+router.post('/forgot_password', [
+    check('mail').isEmail().isEmpty()
+], async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    let data = req.body;
     res.json(await user_ctr.forgot_password(req.body.email));
 });
 
-router.post('/change_password', middle_auth.validate, async function (req, res) {
+//Cambiar contraseña. CONFIRMAR 
+router.post('/change_password', middle_auth.validate
+[
+   check(middle_auth).isEmpty()
+], async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(422).json({ errors: errors.array() });
+    }
     let data = req.body;
     res.json(await user_ctr.change_password(data));
 });
