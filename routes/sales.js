02b6b11935e1fe5e30a410ce25d977fbb49aaacf
JP-GITHUB <joseluis.p10@hotmail.com>
2018-12-03 02:50:15 -0300

    Listando ventas por usuario

--- routes/sales.js
+++ routes/sales.js
@@ -14,8 +14,12 @@ router.post('/deliver', middle_auth.validate, middle_auth.verify_permisson, asyn
 	res.json(await sales_ctr.deliver(code));
 });
 
-router.get('/:code', middle_auth.validate, middle_auth.verify_permisson, async function (req, res) {
+router.get('/code/:code', middle_auth.validate, middle_auth.verify_permisson, async function (req, res) {
 	res.json(await sales_ctr.get_by_code(req.params.code));
+});
+
+router.get('/user_email/:email', middle_auth.validate, middle_auth.verify_permisson, async function (req, res) {
+	res.json(await sales_ctr.get_by_user_email(req.params.email));
 });
 
 module.exports = router;
